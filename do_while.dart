8549e3d7a8b663cd57b4094ce1a21f82f8e3ecb7
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-04 01:17:21 +0500

    do while loop added

--- do_while.dart
+++ do_while.dart
@@ -3,6 +3,6 @@ import 'main.dart';
 void main(List<String> args) {
   int count = 11;
   do {
-    print('The Loop is running while condition is false ${count++}');
+    print('The Loop is running while condition is false ${++count}');
   } while (count <= 10);
 }
