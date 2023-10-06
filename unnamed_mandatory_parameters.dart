4395bb1bf5829e46b6bb9c83a4a6c611658f0aa2
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-06 16:44:45 +0500

    Unnamed Mandatory Parameters

---
+++ unnamed_mandatory_parameters.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  tea(2, 2);
+}
+
+tea(int sugarSpoon, int teaCups) {
+  String from = 'canteen';
+  print(
+      "Bring $teaCups cups of tea with $sugarSpoon spoon of sugar from $from");
+}
