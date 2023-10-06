e6a4e8b00144936760e965a090e89455d18b8c66
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-06 16:28:55 +0500

    Simple function

---
+++ simple_function.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  tea(2);
+}
+
+tea(int sugar) {
+  int teaCups = 2;
+  String from = 'canteen';
+  print("Bring $teaCups cups of tea with $sugar spoon of sugar from $from");
+}
