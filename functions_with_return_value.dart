22d7f54a872f225a3bfacff49a3eb340040e26ac
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-11 19:41:58 +0500

    When return value of a bool function is true

---
+++ functions_with_return_value.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool teaSignal = IsTeaCupReady(2, 2);
+  if (teaSignal) {
+    print('Thanks for the tea');
+  } else {
+    print('I would like a cup of tea please');
+  }
+}
+
+bool IsTeaCupReady(int numOfTeaCups, int numOfSugarS) {
+  print('A cup of tea');
+  return true;
+}
