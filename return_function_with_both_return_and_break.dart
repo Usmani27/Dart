8af8fcb5a8595d6c533c3c96bc5c6c478baf190f
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-12 02:33:17 +0500

    The use of both return and break keywords in the return function with multiple conditions

---
+++ return_function_with_both_return_and_break.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  while (true) {
+    if (!isTeaAvailable()) {
+      print("The tea is available");
+      return;
+    } else
+      break;
+  }
+  bool teaSignal = isTeaCupReady(2, 2);
+  if (teaSignal) {
+    print("Thanks for the tea");
+  } else {
+    print("I would like to have a cup of tea");
+  }
+}
+
+bool isTeaCupReady(int numberOfTeaCups, int numberOfSugars) {
+  print("A cup of tea please");
+  return true;
+}
+
+bool isTeaAvailable() {
+  int sugarPacks = 2;
+  int teaPacks = 2;
+  bool isGasAvailable = true;
+  return sugarPacks > 0 && teaPacks == 2 && isGasAvailable;
+}
