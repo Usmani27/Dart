8faaa032e490c376c3476a5d43a3c38fb7a7819c
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-12 00:57:24 +0500

    The return keyword won't let the compiler read any line below it

---
+++ return_function_with_return_keyword.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  if (!isReturnAvailable()) {
+    print("It means the tea is not available");
+  }
+  return;
+
+  if (isTeaAvailable()) {
+    print(
+        "The return keyword above won't let it print even the condition is true");
+  } else {
+    print("It won't print");
+  }
+}
+
+bool isReturnAvailable() {
+  return false;
+}
+
+bool isTeaAvailable() {
+  return true;
+}
