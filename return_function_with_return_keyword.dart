34fc3d29018f0e426b8f1fdecb6a329e0d27f40c
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-12 01:00:47 +0500

    The return keyword will behave the same way in the while loop too

--- return_function_with_return_keyword.dart
+++ return_function_with_return_keyword.dart
@@ -1,14 +1,16 @@
 void main(List<String> args) {
-  if (!isReturnAvailable()) {
-    print("It means the tea is not available");
-  }
-  return;
+  while (true) {
+    if (!isReturnAvailable()) {
+      print("It means the tea is not available");
+    }
+    return;
 
-  if (isTeaAvailable()) {
-    print(
-        "The return keyword above won't let it print even the condition is true");
-  } else {
-    print("It won't print");
+    if (isTeaAvailable()) {
+      print(
+          "The return keyword above won't let it print even the condition is true");
+    } else {
+      print("It won't print");
+    }
   }
 }
 
