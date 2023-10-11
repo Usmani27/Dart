b94efb787971e0bc49f0d6c55912fe87c09a2d90
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-11 16:11:17 +0500

    File Name Udated

---
+++ adding_empty_list.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  List<String> names = [];
+  print('**********Menue Options**********');
+  print('******Enter 1 to add the name*******');
+  print('****Eneter 2 to print the name with serial number');
+
+  while (true) {
+    print('Please select from the options 1 or 2');
+    String option = stdin.readLineSync()!;
+    if (option == '1') {
+      print('Please enter your name');
+      String yourName = stdin.readLineSync()!;
+      names.add(yourName);
+    } else if (option == '2') {
+      int sno = 1;
+      for (String name in names) {
+        print("Serial No.: ${sno++} Name: $name");
+      }
+    } else {
+      return;
+    }
+  }
+}
