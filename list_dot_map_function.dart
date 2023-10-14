64b6425ba4201e8e100e4c270ac264e95fb55b89
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-14 18:46:08 +0500

    By using the variable to store frist index of a string value

--- list_dot_map_function.dart
+++ list_dot_map_function.dart
@@ -1,17 +1,37 @@
-void main(List<String> args) {
-  List<String> foods = ["Burger", "Pizza", "Roll"];
-  foods.add("Sandwich");
-  print(foods);
+void main() {
+  List<String> foods = ['Burger', 'Fries', 'Pizza'];
 
-  for (String food in foods) {
-    print("The food from the for loop is: $food");
+  foods.add('Donut');
+
+  for (String fastFood in foods) {
+    print('$fastFood from for loop');
   }
-  print("*****************");
 
-  List<String> firstCharacterOfEveryFood = foods.map(returnFirstIndex).toList();
-  print(firstCharacterOfEveryFood);
+  print(foods.map((e) => e[0]));
+
+  List<String> firstcharacterOfEveryFood =
+      foods.map(returnFirstIndexAlphabet).toList();
+  print(firstcharacterOfEveryFood);
+  List<String> dublicateFoods = [];
+  for (String food in foods) {
+    dublicateFoods.add(food[0]);
+  }
+  print('Result of duplicateFoods $dublicateFoods');
+  List<String> dublicateFoods2 = [];
+  for (String food in foods) {
+    dublicateFoods2.add(returnFirstIndexAlphabet(food));
+  }
+  print('Result of duplicateFoods2 $dublicateFoods2');
+
+  List<String> dublicateFoods3 = [];
+  for (String food in foods) {
+    var functinInVariable = returnFirstIndexAlphabet(food);
+    dublicateFoods3.add(functinInVariable);
+    print('Value of functinInVariable: $functinInVariable');
+  }
+  print('Result of duplicateFoods3 $dublicateFoods3');
 }
 
-String returnFirstIndex(food) {
+String returnFirstIndexAlphabet(food) {
   return food[0];
 }
