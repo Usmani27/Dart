fcac42694e46d8b13ac648e91a018da47e97e944
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-16 22:33:02 +0500

    Constructor parameter and class variable have same name the output is empty

--- constructor.dart
+++ constructor.dart
@@ -1,28 +1,22 @@
 void main(List<String> args) {
-  print("*****Constructor With Default Optional Positional Parameter*****");
-  Cars toyota = Cars(brandName: "Corrolla", company: "Toyota");
-  // toyota.carBrand = "Corrola";
-  //toyota.companyName = "Toyota";
+  print("*Constructor with same parameter and variable name with Argment*");
+  Cars toyota = Cars(carBrand: "Corrolla");
   print(toyota.carBrand);
   toyota.engine();
-  print("*****Consturctor With Optional Positional Parameter*****");
-  Cars honda = Cars(brandName: "Civic", company: "Honda");
-  //honda.carBrand = "Civic";
-  // honda.companyName = "Honda";
-  print("${honda.carBrand} is the car name");
+  print("*Constructor with same parameter and variable name with Argment*");
+  Cars honda = Cars();
+  print(toyota.carBrand);
   honda.engine();
 }
 
 class Cars {
-  String carBrand = 'Motor Car';
-  String companyName = "Motor Company";
+  String carBrand = '';
 
-  Cars({String brandName = "Model Name", String company = "Company Name"}) {
-    carBrand = brandName;
-    companyName = company;
+  Cars({carBrand = "Model Name"}) {
+    carBrand = carBrand;
   }
 
   engine() {
-    print("The engine of $companyName $carBrand is automatic");
+    print("The engine is automatic $carBrand");
   }
 }
