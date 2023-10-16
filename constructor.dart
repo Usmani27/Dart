313446acc3259b439b06435a4663f0898e596fd3
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-17 00:02:44 +0500

    The body of the constructor {} will execute automatically

--- constructor.dart
+++ constructor.dart
@@ -1,22 +1,19 @@
 void main(List<String> args) {
-  print("*Constructor with same parameter and variable name with Argment*");
+  print("***First Instance***");
   Cars toyota = Cars(carBrand: "Corrolla");
   print(toyota.carBrand);
-  toyota.engine();
-  print("*Constructor with same parameter and variable name with Argment*");
-  Cars honda = Cars();
+
+  print("***Second Instance***");
+  Cars honda = Cars(engineCC: "5000");
   print(honda.carBrand);
-  honda.engine();
 }
 
 class Cars {
   String carBrand = '';
   String engineCC = "";
 
-  Cars({this.carBrand = "Model Name", this.engineCC = ""}) {}
-
-  engine() {
-    print("The name of the car is $carBrand");
-    print("The engine size of the car is $engineCC");
+  Cars({this.carBrand = "Model Name", this.engineCC = "3000"}) {
+    print("This is the name of car in your instance. Car Name: $carBrand");
+    print("THis is the engine size in your instance. Engine Size: $engineCC");
   }
 }
