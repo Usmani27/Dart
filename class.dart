8e261e1ec1b948a7b74a6b6ab968fc9e8e5db378
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-16 17:16:08 +0500

    How to create multiple intances out of one  calss without making any change in the main class objects

--- class.dart
+++ class.dart
@@ -1,12 +1,21 @@
 void main(List<String> args) {
+  print("*****Class Cars First Intance*****");
   Cars toyota = Cars();
+  toyota.carBrand = "Corrola";
+  toyota.companyName = "Toyota";
   print(toyota.carBrand);
   toyota.engine();
+  print("*****Class Cars First Intance*****");
+  Cars honda = Cars();
+  honda.carBrand = "Civic";
+  honda.companyName = "Honda";
+  print("${honda.carBrand} is the car name");
+  honda.engine();
 }
 
 class Cars {
-  String carBrand = 'Corrolla';
-  String companyName = "Toyota";
+  String carBrand = 'Motor Car';
+  String companyName = "Motor Company";
 
   engine() {
     print("The engine of $companyName $carBrand is automatic");
