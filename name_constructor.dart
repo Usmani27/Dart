17f8a3bda6734f6014399d955379f345f4685c66
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-17 01:02:52 +0500

    We can create an instance by name constructor as well the other defalut instances will refer to default constructor where as the instance created by the name constructor will refer to name constructor's parameters

---
+++ name_constructor.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  print("***Name Constructor***");
+
+  Cars bus = Cars.engine(engineCC: "10000");
+  print(bus.engineCC);
+
+  print("***First Default Instance***");
+  Cars toyota = Cars(carName: "Corrolla");
+  print(toyota.carName);
+
+  print("***Second Default Instance***");
+  Cars honda = Cars(engineCC: "5000");
+  print(honda.carName);
+}
+
+class Cars {
+  String carName = 'Motor Car';
+  String engineCC = "2000";
+
+  Cars({this.carName = "Model Name", this.engineCC = "3000"}) {}
+
+  Cars.engine({this.engineCC = "1500"}) {}
+}
