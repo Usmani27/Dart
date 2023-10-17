4ee269c3d1380f457f1fa9a43862238e843611d2
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-17 22:09:30 +0500

    By using theset keyword we can control the user input

--- getters_setters.dart
+++ getters_setters.dart
@@ -1,10 +1,22 @@
 void main(List<String> args) {
   Finance finance = Finance();
-  finance.addFunction(110);
+  // finance.addFunction(-10);
+  finance.addSetter = -10;
+  print(finance.add);
 }
 
 class Finance {
   int add = 0;
+
+  set addSetter(int setAdd) {
+    if (setAdd < 0) {
+      print("the value must be greater than 10 or above");
+      setAdd = 10;
+    } else {
+      print("The value of setAdd is greater than 10 > $setAdd");
+    }
+    add = setAdd;
+  }
 
   addFunction(int add) {
     if (add > 0) {
