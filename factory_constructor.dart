47008b3de64f81c2a05ed19e6ffce8068cc036d9
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-17 19:11:41 +0500

    Factory Constructor and default constructor together

--- factory_constructor.dart
+++ factory_constructor.dart
@@ -1,17 +1,29 @@
 void main(List<String> args) {
+  print("*******Factory Constructor********");
   Contacts contact =
       Contacts.factoryConstructor("Kaashaann", "email@email.com");
   print(contact.name);
 
   print(contact.email);
+
+  print(contact.address);
+
+  print("*******Default Constructor********");
+  Contacts defaultContact = Contacts(
+      name: "Mauhaamed",
+      address: "Default Constructor's Argument Address",
+      email: "fawad&fawad.com");
+  print(defaultContact.name);
+  print(defaultContact.address);
+  print(defaultContact.email);
 }
 
 class Contacts {
   String name = "";
-  String address = "";
+  String address = "Class Address";
   String email = "";
 
-  Contacts();
+  Contacts({this.name = "", this.address = "", this.email = ""});
 
   factory Contacts.factoryConstructor(String factoryName, String factoryEmail) {
     var newInstance = Contacts();
