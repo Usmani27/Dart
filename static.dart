f84f0130c430226c5e46278d56c71921e93734ff
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-17 20:33:18 +0500

    We can not access static variable of the class from the instance we can only acces through the class Student.country

---
+++ static.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  Student student = Student(
+    name: "Amir",
+    contact: "0300",
+  );
+  print(student.name);
+  print(student.contact);
+  print(Student.country);
+
+  Student student2 = Student(
+    name: "Fawad",
+    contact: "0301",
+  );
+  print(student2.name);
+  print(student2.contact);
+  print(student2.country);
+}
+
+class Student {
+  String name = "";
+
+  String contact = "";
+  static String country = "";
+
+  Student({
+    this.name = "",
+    this.contact = "",
+  }) {
+    country = "Pakistan";
+  }
+}
