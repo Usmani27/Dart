01159484d1f79eeaac593d328563b960508342b9
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-18 23:47:44 +0500

    This code creates a Singleton class that can only be instantiated once, and provides a factory constructor to get the instance of the class. The main method creates two objects of the Singleton class, and prints the hashcode of the objects to verify that they are same.

---
+++ singleton.dart
@@ -0,0 +1,18 @@
+void main() {
+  Singleton obj1 = Singleton();
+  Singleton obj2 = Singleton();
+  print(obj1.hashCode);
+  print(obj2.hashCode);
+}
+
+class Singleton {
+  // static variable
+  static final Singleton _instance = Singleton._internal();
+
+// factory constructor
+  factory Singleton() {
+    return _instance;
+  }
+  // private constructor
+  Singleton._internal();
+}
