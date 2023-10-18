932044eae48830346c94bbf6909448462c2f71b5
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-18 17:25:47 +0500

    when we override the parent function in the child class then the child class method will supersede the parent class method if any changes have been made in the child class then those changes will be perminent.

---
+++ polymorphism.dart
@@ -0,0 +1,36 @@
+void main(List<String> args) {
+  Men men = Men("Male", "Fawad", 30);
+  men.dayWork();
+  print("***Human class output of function dayWork() starts here***");
+  Human().dayWork();
+  print("***Human class output of function dayWork() ends here***");
+  print(men.gender);
+  print(men.age);
+  print(men.name);
+}
+
+class Human {
+  String name = "";
+  int age = 0;
+
+  Human({this.name = "", this.age = 10}) {}
+
+  dayWork() {
+    print("Eat");
+    print("Sleep");
+    print("Servive");
+  }
+}
+
+class Men extends Human {
+  String gender = "";
+
+  Men(this.gender, String manName, int manAge)
+      : super(name: manName, age: manAge) {}
+  @override
+  dayWork() {
+    print("Eat");
+    print("Sleep");
+    print("work");
+  }
+}
