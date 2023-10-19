e9fe1ef61bdcdf8b7eb4730cf4cebb85bb97d539
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-19 16:49:29 +0500

    Even the class that is inheriting from the normal class that iherits from the abstract will have to implements the methods those are parts of concrete implementation

--- abstraction.dart
+++ abstraction.dart
@@ -4,31 +4,11 @@ void main(List<String> args) {
   Test.testFunction();
 }
 
-class Test2 {
-  String name = "abc";
-  void testFunction();
-  void testFunction2() {
-    print("Testing");
-  }
-}
-
 abstract class Test {
-  String name = "abc";
+  String name = "";
+
   void testFunction();
   void testFunction2() {
     print("Testing");
-  }
-}
-
-class childClass extends Test {
-  @override
-  void testFunction() {
-    // TODO: implement testFunction
-  }
-
-  @override
-  void testFunction2() {
-    // TODO: implement testFunction2
-    super.testFunction2();
   }
 }
