25e9b3fc8122864e6a5ade23e916cf4e286606d5
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-20 16:13:25 +0500

    'Mixin on' ca be applied on abstract class only the child class of that class can access mixin on. Mix in on allows to access the functional part of the abstract method. We create mixin on abstract class and write the functional part. The child class of the abstract can access this function directly on the call site through its instance. The child class doesn't need to impliment the mixin function on the class site. THe child class will only used 'mixin with' while being created.

---
+++ mixin_on.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  var dog = Dog("My Dog Onyx", 25.5);
+  dog.run();
+}
+
+abstract class Animal {
+  String name;
+  double speed;
+
+  Animal(this.name, this.speed);
+
+  void run();
+}
+
+//No Concrete Implementation when use mixin on Abstract Class
+
+mixin CanRun on Animal {
+  @override
+  void run() => print("$name runs at the speed of $speed km");
+}
+
+class Dog extends Animal with CanRun {
+  Dog(String name, double speed) : super(name, speed);
+}
