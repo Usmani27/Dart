65ba6fceff76ddfeeb5788d0a97653ca776090c1
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-21 23:44:39 +0500

    How to handle RangeError with exception handling

--- exception_handling.dart
+++ exception_handling.dart
@@ -1,11 +1,25 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  print("Please enter your age");
+  List<String> friends = ["Amir", "Ali"];
   try {
-    int age = int.parse(stdin.readLineSync()!);
-    print(age);
+    print(friends[10]);
   } catch (e) {
-    print(e);
+    print("Here is the ERROR $e");
   }
 }
+
+
+
+
+
+
+// void main(List<String> args) {
+//   print("Please enter your age");
+//   try {
+//     int age = int.parse(stdin.readLineSync()!);
+//     print(age);
+//   } catch (e) {
+//     print("here is the main class of error:==> $e");
+//   }
+// }
