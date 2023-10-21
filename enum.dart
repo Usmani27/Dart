efb6475647883793beefea38f2b5f38a87c26ed5
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-21 20:43:18 +0500

    How to Print All Enum Values. Days.values: It returns all the values of the enum.

--- enum.dart
+++ enum.dart
@@ -1,35 +1,45 @@
-void main(List<String> args) {
-  Image().pick(PickSource.camra);
-}
+enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
 
-enum PickSource { camra, mobile, google }
-
-class Image {
-  pick(PickSource imageType) {
-    switch (imageType) {
-      case PickSource.camra:
-        print("This is image is taken from ${PickSource.camra}");
-        break;
-      case PickSource.mobile:
-        print("This is image is taken from ${PickSource.mobile}");
-        break;
-      case PickSource.google:
-        print("This is image is taken from ${PickSource.google}");
-        break;
-    }
-    //"***One of the Way to Convet Enum Type to String Type***
-
-    String type = '';
-    if (imageType == PickSource.camra) {
-      type = 'gallary';
-      print("Enum type to String Type: $type");
-    }
-    //*******Enum.values for 'for in loop'**** */
-    for (PickSource abc in PickSource.values) {
-      print(abc);
-    }
+void main() {
+  // Days.values: It returns all the values of the enum.
+  for (Days day in Days.values) {
+    print(day);
   }
 }
+
+// void main(List<String> args) {
+//   Image().pick(PickSource.camra);
+// }
+
+// enum PickSource { camra, mobile, google }
+
+// class Image {
+//   pick(PickSource imageType) {
+//     switch (imageType) {
+//       case PickSource.camra:
+//         print("This is image is taken from ${PickSource.camra}");
+//         break;
+//       case PickSource.mobile:
+//         print("This is image is taken from ${PickSource.mobile}");
+//         break;
+//       case PickSource.google:
+//         print("This is image is taken from ${PickSource.google}");
+//         break;
+//     }
+//     //"***One of the Way to Convet Enum Type to String Type***
+
+//     String type = '';
+//     if (imageType == PickSource.camra) {
+//       type = 'gallary';
+//       print("Enum type to String Type: $type");
+//     }
+
+//     //*******Enum.values for 'for in loop'**** */
+//     for (PickSource abc in PickSource.values) {
+//       print(abc);
+//     }
+//   }
+// }
 
 
 
