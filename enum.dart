e24eedfb64c78a1728b9f87fd346b42a3c18d276
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-21 19:41:39 +0500

    This is one of the way to convert enum type value to string type value.

--- enum.dart
+++ enum.dart
@@ -1,19 +1,79 @@
-enum Gender { Male, Female, Others }
+void main(List<String> args) {
+  Image().pick(PickSource.camra);
+}
 
-class GenderType {
-  String name;
-  int contact;
-  Gender gender;
+enum PickSource { camra, mobile, google }
 
-  GenderType(this.name, this.contact, this.gender) {
-    print("this is the name $name, contact $contact and gender $gender");
+class Image {
+  pick(PickSource imageType) {
+    switch (imageType) {
+      case PickSource.camra:
+        print("This is image is taken from ${PickSource.camra}");
+        break;
+      case PickSource.mobile:
+        print("This is image is taken from ${PickSource.mobile}");
+        break;
+      case PickSource.google:
+        print("This is image is taken from ${PickSource.google}");
+        break;
+    }
+    //"***One of the Way to Convet Enum Type to String Type***
+
+    String type = '';
+    if (imageType == PickSource.camra) {
+      type = 'gallary';
+      print("Enum type to String Type: $type");
+    }
   }
 }
 
-void main(List<String> args) {
-  GenderType gendertype = GenderType("Amir", 30, Gender.Male);
-  print(gendertype.gender);
-}
+
+
+
+
+
+// enum CompanyType {
+//   soleProprietorship("Sole Proprietorship"),
+//   partnership("Partnership"),
+//   corporation("Corporation"),
+//   limitedLiabilityCompany("Limited Liability Company");
+
+//   // Members
+//   final String text;
+//   const CompanyType(this.text);
+// }
+
+// void main() {
+//   CompanyType soleProprietorship = CompanyType.soleProprietorship;
+//   print(soleProprietorship.text);
+//   String enumValue = soleProprietorship.text;
+//   print(enumValue);
+// }
+
+
+
+
+
+
+
+
+
+// enum Gender { Male, Female, Others }
+
+// class GenderType {
+//   String name;
+//   int contact;
+//   Gender gender;
+
+//   GenderType(this.name, this.contact, this.gender) {
+//     print("this is the name $name, contact $contact and gender $gender");
+//   }
+// }
+
+// void main(List<String> args) {
+//   GenderType gendertype = GenderType("Amir", 30, Gender.Male);
+//   print(gendertype.gender);
+// }
 
 
 // void main(List<String> args) {
