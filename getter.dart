305ebba9807d80b6438dd2e6f039146149e9ac8e
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-22 13:42:21 +0500

    Getter with private property. We use ! to remove the error

--- getter.dart
+++ getter.dart
@@ -1,3 +1,13 @@
+class Book {
+  String? _name;
+  int? _prize;
+
+  Book(this._name, this._prize);
+
+  String get name => this._name!;
+  int get prize => this._prize!;
+}
+
 class Person {
   String firstname;
   String lastname;
@@ -8,6 +18,10 @@ class Person {
 }
 
 void main(List<String> args) {
+  Book book = Book("Kitab", 50);
+  print(book.name);
+  print(book.prize);
+
   Person person = Person("Fawad", "Ahmed");
   print(person.fullname);
 }
