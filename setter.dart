913367c6f934e01796b8c46453f8b728f50ff5ea
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-22 12:55:13 +0500

    The classnumber setter will only accept a value between 1 and 12. We will use 'throw' keyword to show the custom exception

--- setter.dart
+++ setter.dart
@@ -1,3 +1,33 @@
+void main(List<String> args) {
+  Student student = Student();
+  student.name = "Joe";
+  student.classnumber = 13;
+  student.display();
+
+  Book book = Book();
+  book.name = "'Setter'";
+  book.prize = 10;
+  book.dispaly();
+}
+
+class Student {
+  String? _name;
+  int? _classnumber;
+
+  set name(String name) => this._name = name;
+  set classnumber(int classnumber) {
+    if (classnumber < 1 || classnumber > 12) {
+      throw ("The class number must be 1 to 12");
+    }
+    this._classnumber = classnumber;
+  }
+
+  void display() {
+    print("Name: $_name");
+    print("Class Number: $_classnumber");
+  }
+}
+
 class Book {
   String? _name;
   double? _prize;
@@ -15,11 +45,4 @@ class Book {
     print(
         "The name of the booke is $_name and the prize of the book is Rs.$_prize");
   }
-}
-
-void main(List<String> args) {
-  Book book = Book();
-  book.name = "'Setter'";
-  book.prize = -10;
-  book.dispaly();
 }
