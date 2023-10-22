bd9bce5e6e6d1430af2753420a3857d1e0cb82df
Fawad Ahmed <usmani.fawad@gmail.com>
2023-10-22 14:07:44 +0500

    Getter can be applied on map as well.

--- getter.dart
+++ getter.dart
@@ -1,3 +1,31 @@
+void main(List<String> args) {
+  Contact contact = Contact("Amir", 40, "Male");
+  print(contact.map);
+
+  Book book = Book("", 10);
+  print(book.name);
+  print(book.prize);
+
+  Person person = Person("Fawad", "Ahmed");
+  print(person.fullname);
+}
+
+class Contact {
+  String _name;
+  int _age;
+  String _gender;
+
+  Contact(this._name, this._age, this._gender);
+
+  String get name => this._name;
+  int get age => this._age;
+  String get gender => this.gender;
+
+  Map<String, dynamic> get map {
+    return {"name": _name, "age": _age, "gender": _gender};
+  }
+}
+
 class Book {
   String _name;
   int _prize;
@@ -21,13 +49,4 @@ class Person {
   Person(this.firstname, this.lastname);
 
   String get fullname => ("$firstname $lastname");
-}
-
-void main(List<String> args) {
-  Book book = Book("", 10);
-  print(book.name);
-  print(book.prize);
-
-  Person person = Person("Fawad", "Ahmed");
-  print(person.fullname);
 }
